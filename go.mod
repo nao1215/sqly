4a561762c0a99c1a28fd86b7d5a8dd32aeb5415e
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2022-11-02 01:25:36 +0900

    Add interactive input

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/nao1215/sqly
 
 go 1.19
 
-require github.com/mattn/go-sqlite3 v1.14.16
+require (
+	github.com/fatih/color v1.13.0
+	github.com/mattn/go-colorable v0.1.13
+	github.com/mattn/go-sqlite3 v1.14.16
+	github.com/mattn/go-tty v0.0.4
+)
+
+require (
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+)
--- main.go
+++ main.go
@@ -6,14 +6,23 @@ import (
 	"log"
 	"os"
 
+	"github.com/fatih/color"
+	"github.com/mattn/go-colorable"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/mattn/go-tty"
 	"github.com/nao1215/sqly/infrastructure/persistence/csv"
 	"github.com/nao1215/sqly/infrastructure/persistence/sqlite3"
 	"github.com/nao1215/sqly/usecase"
 )
 
-// Version is sqly command version. Version value is assigned by LDFLAGS.
-var Version string
+var (
+	// Version is sqly command version. Version value is assigned by LDFLAGS.
+	Version string
+	// Stdout is new instance of Writer which handles escape sequence for stdout.
+	Stdout = colorable.NewColorableStdout()
+	// Stderr is new instance of Writer which handles escape sequence for stderr.
+	Stderr = colorable.NewColorableStderr()
+)
 
 func main() {
 	os.Remove("./foo.db")
@@ -40,9 +49,38 @@ func main() {
 		log.Fatal(err)
 	}
 
+	printWelcomeMessage()
+	interactive()
+
 	if err := sqlite3Inreractor.Exec("SELECT 配達区分 FROM sample;"); err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println("success")
+}
+
+func printWelcomeMessage() {
+	fmt.Fprintf(Stdout, "%s %s\n", color.GreenString("sqly"), Version)
+	fmt.Fprintf(Stdout, "enter %s for usage hints.\n", color.CyanString("\".help\""))
+}
+
+func interactive() {
+	tty, err := tty.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tty.Close()
+
+	fmt.Fprintf(Stdout, "%s>>", color.GreenString("sqly"))
+	input := ""
+	for {
+		r, err := tty.ReadRune()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Enter押下したかの判定と補完処理を追加する
+		input += string(r)
+		fmt.Fprintf(Stdout, "\r%s>>%s", color.GreenString("sqly"), input)
+	}
 }
