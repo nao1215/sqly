6f3941488e4b04e35ae3744dce7526d13ed6bc54
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2022-11-09 12:29:29 +0900

    Add .header command

---
+++ shell/header.go
@@ -0,0 +1,42 @@
+package shell
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/nao1215/sqly/domain/model"
+	"github.com/olekukonko/tablewriter"
+)
+
+// headerCommand print table header.
+func (c CommandList) headerCommand(s *Shell, argv []string) error {
+	if len(argv) == 0 {
+		fmt.Fprintln(Stdout, "[Usage]")
+		fmt.Fprintln(Stdout, "  .header TABLE_NAME")
+		return nil
+	}
+
+	table, err := s.sqlite3Interactor.List(s.Ctx, argv[0])
+	if err != nil {
+		return err
+	}
+	table.Name = argv[0]
+	printHeader(os.Stdout, table)
+	return nil
+}
+
+// printHeader print header
+func printHeader(out *os.File, t *model.Table) {
+	tableData := [][]string{}
+	tableData = append(tableData, []string{t.Name})
+
+	table := tablewriter.NewWriter(out)
+	table.SetHeader([]string{t.Name})
+	table.SetAutoWrapText(false)
+	table.SetAutoFormatHeaders(false)
+
+	for _, v := range t.Header {
+		table.Append([]string{v})
+	}
+	table.Render()
+}
