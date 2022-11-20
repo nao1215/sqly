0e388038b82f78ccd55c057e80e5b2614a31e2cf
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2022-11-20 22:29:44 +0900

    Add unit test for config

---
+++ config/config_test.go
@@ -0,0 +1,30 @@
+// Package config manage sqly configuration
+package config
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/nao1215/gorky/file"
+)
+
+func TestConfig_CreateDir(t *testing.T) {
+	t.Run("Create sqly config directory", func(t *testing.T) {
+		homeDir := t.TempDir()
+		t.Setenv("HOME", homeDir)
+
+		c, err := NewConfig()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if err := c.CreateDir(); err != nil {
+			t.Fatal(err)
+		}
+
+		want := filepath.Join(homeDir, ".config", "sqly")
+		if !file.IsDir(want) {
+			t.Errorf("failed to create config directory at %s", want)
+		}
+	})
+}
