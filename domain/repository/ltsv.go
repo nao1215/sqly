a6d4bec4c1048f5bae1651eadbbf34360a512e70
Naohiro CHIKAMATSU <n.chika156@gmail.com>
2026-03-22 15:08:12 +0900

    Remove dead List() methods from CSV/TSV/LTSV repository interfaces
    These List() methods were no longer called after the filesql migration.
    All file reading now goes through the FileSQLAdapter. Only the Dump()
    methods remain as they are still used for file export.

--- domain/repository/ltsv.go
+++ domain/repository/ltsv.go
@@ -10,8 +10,6 @@ import (
 
 // LTSVRepository is a repository that handles LTSV file.
 type LTSVRepository interface {
-	// List get ltsv all data with label.
-	List(ltsv *os.File) (*model.LTSV, error)
 	// Dump write contents of DB table to LTSV file
 	Dump(ltsv *os.File, table *model.Table) error
 }
--- domain/repository/tsv.go
+++ domain/repository/tsv.go
@@ -10,8 +10,6 @@ import (
 
 // TSVRepository is a repository that handles TSV file.
 type TSVRepository interface {
-	// List get tsv all data with header.
-	List(tsv *os.File) (*model.TSV, error)
 	// Dump write contents of DB table to TSV file
 	Dump(tsv *os.File, table *model.Table) error
 }
